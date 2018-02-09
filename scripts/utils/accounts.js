60db74a3c853fd91036fab51998d44887e0b45df
Aleksey Popov <alepooop@gmail.com>
2018-02-09 16:37:02 +0300

    :sparkles: Weight Cap
    Change accounts weight based on the rule close #2

--- scripts/utils/accounts.js
+++ scripts/utils/accounts.js
@@ -1,12 +1,39 @@
-const { exclude } = require('../../config.json');
-const { fromRawLsk } = require('./lisk.js');
+const { exclude, weightCap } = require('../../config.json');
+const { fromRawLsk, toRawLsk } = require('./lisk.js');
+
+const cap = weightCap.sort((a, b) => a > b);
 
 const removeExludedAccounts = accounts =>
     accounts.filter(account => {
-        return !(Boolean(exclude.weightCap) && fromRawLsk(account.balance) >= exclude.weightCap) &&
-            !exclude.addresses.includes(account.address)
+        return (
+            !(
+                Boolean(exclude.weightCap) &&
+                fromRawLsk(account.balance) >= exclude.weightCap
+            ) && !exclude.addresses.includes(account.address)
+        );
     });
 
+const weightCapValidator = balance =>
+    cap
+        .reduce(
+            (prev, current) =>
+                balance <= prev
+                    ? balance
+                    : balance >= prev && balance <= current ? prev : current,
+            cap[0]
+        );
+
+const accountWeightChanger = account => {
+    console.log('pre', fromRawLsk(account.balance))
+    account.balance = toRawLsk(weightCapValidator(fromRawLsk(account.balance)));
+    console.log('post', fromRawLsk(account.balance))
+    return account;
+};
+
+const validateAccauntWeight = accounts =>
+    cap.length > 0 ? accounts.map(accountWeightChanger) : accounts;
+
 module.exports = {
-    removeExludedAccounts
-}
+    removeExludedAccounts,
+    validateAccauntWeight,
+};
