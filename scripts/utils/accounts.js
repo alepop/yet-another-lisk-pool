48d4883bc8a832cbb5a61f13d42a161a21f178b0
Tobias Lins <me@tobi.sh>
2018-10-26 15:58:21 +0200

    fix voters request to receive all voters due 1.0 api change

--- scripts/utils/accounts.js
+++ scripts/utils/accounts.js
@@ -1,37 +1,38 @@
-const { exclude, weightCap } = require('../../config.json');
-const { fromRawLsk, toRawLsk } = require('./lisk.js');
+const { exclude, weightCap } = require("../../config.json");
+const { fromRawLsk, toRawLsk } = require("./lisk.js");
 
 const cap = weightCap.sort((a, b) => a > b);
 
 const removeExludedAccounts = accounts =>
-    accounts.filter(account => {
-        return (
-            !(
-                Boolean(exclude.weightCap) &&
-                fromRawLsk(account.balance) >= exclude.weightCap
-            ) && !exclude.addresses.includes(account.address)
-        );
-    });
+  accounts.filter(account => {
+    return (
+      !(
+        Boolean(exclude.weightCap) &&
+        fromRawLsk(account.balance) >= exclude.weightCap
+      ) && !exclude.addresses.includes(account.address)
+    );
+  });
 
 const weightCapValidator = balance =>
-    cap
-        .reduce(
-            (prev, current) =>
-                balance <= prev
-                    ? balance
-                    : balance >= prev && balance <= current ? prev : current,
-            cap[0]
-        );
+  cap.reduce(
+    (prev, current) =>
+      balance <= prev
+        ? balance
+        : balance >= prev && balance <= current
+          ? prev
+          : current,
+    cap[0]
+  );
 
 const accountWeightChanger = account => {
-    account.balance = toRawLsk(weightCapValidator(fromRawLsk(account.balance)));
-    return account;
+  account.balance = toRawLsk(weightCapValidator(fromRawLsk(account.balance)));
+  return account;
 };
 
-const validateAccauntWeight = accounts =>
-    cap.length > 0 ? accounts.map(accountWeightChanger) : accounts;
+const validateAccountWeight = accounts =>
+  cap.length > 0 ? accounts.map(accountWeightChanger) : accounts;
 
 module.exports = {
-    removeExludedAccounts,
-    validateAccauntWeight,
+  removeExludedAccounts,
+  validateAccountWeight
 };
--- scripts/utils/api.js
+++ scripts/utils/api.js
@@ -1,46 +1,78 @@
-const { APIClient } = require('lisk-elements').default;
-const { fromRawLsk, getTotalVoteWeight } = require('./lisk.js');
-const { removeExludedAccounts, validateAccauntWeight } = require('./accounts.js');
-const config = require('../../config.json');
+const { APIClient } = require("lisk-elements").default;
+const { fromRawLsk, getTotalVoteWeight } = require("./lisk.js");
+const {
+  removeExludedAccounts,
+  validateAccountWeight
+} = require("./accounts.js");
+const config = require("../../config.json");
 
-const node = config.node && `${config.node}:${config.port}`
+const node = config.node && `${config.node}:${config.port}`;
 
-const client = config.isTestnet ?
-    APIClient.createTestnetAPIClient({node}) :
-    APIClient.createMainnetAPIClient({node});
+const client = config.isTestnet
+  ? APIClient.createTestnetAPIClient({ node })
+  : APIClient.createMainnetAPIClient({ node });
 
 const getRewards = async (fromTimestamp, toTimestamp) => {
-    console.log('Get forging data...');
-    // Get delegate address for next request
-    const { data: [ delegate ]} = await client.delegates.get({ publicKey: config.delegatePubKey});
-    const { address } = delegate.account;
+  console.log("Get forging data...");
+  // Get delegate address for next request
+  const {
+    data: [delegate]
+  } = await client.delegates.get({ publicKey: config.delegatePubKey });
+  const { address } = delegate.account;
 
-    const { data } = await client.delegates.getForgingStatistics(address, { fromTimestamp, toTimestamp })
-    const reward = fromRawLsk(data.rewards);
-    const sharingReward = reward * config.sharedPercent / 100;
-    return {
-        reward,
-        sharingReward,
-    };
+  const { data } = await client.delegates.getForgingStatistics(address, {
+    fromTimestamp,
+    toTimestamp
+  });
+  const reward = fromRawLsk(data.rewards);
+  const sharingReward = (reward * config.sharedPercent) / 100;
+  return {
+    reward,
+    sharingReward
+  };
+};
+
+const getAllVoters = async () => {
+  let currentOffset = 0;
+  let voters = [];
+  let keepGoing = true;
+
+  while (keepGoing) {
+    const { data } = await client.voters.get({
+      publicKey: config.delegatePubKey,
+      limit: 50,
+      offset: currentOffset
+    });
+    voters.push.apply(voters, data.voters);
+    if (data.votes <= currentOffset) {
+      keepGoing = false;
+    } else {
+      currentOffset += 50;
+    }
+  }
+  return voters;
 };
 
 const getAccountsAndTotalVoteWeight = async () => {
-    console.log('Get accounts data...');
-    const { data: { voters }} = await client.voters.get({publicKey: config.delegatePubKey});
+  console.log("Get accounts data...");
 
-    const filteredAccounts = removeExludedAccounts(voters); // Exlude accounts based on config
-    const validatedAccounts = validateAccauntWeight(filteredAccounts); // Change accaunts weight
+  const voters = await getAllVoters();
 
-    console.log('Calculate total vote weight...');
-    const totalWeight = getTotalVoteWeight(filteredAccounts);
-    return {
-        accounts: filteredAccounts,
-        totalWeight,
-    };
+  console.log(`Found ${voters.length} voters`);
+
+  const filteredAccounts = removeExludedAccounts(voters); // Exlude accounts based on config
+  const validatedAccounts = validateAccountWeight(filteredAccounts); // Change accounts weight
+
+  console.log("Calculate total vote weight...");
+  const totalWeight = getTotalVoteWeight(filteredAccounts);
+  return {
+    accounts: filteredAccounts,
+    totalWeight
+  };
 };
 
 module.exports = {
-    getAccountsAndTotalVoteWeight,
-    getRewards,
-    client
+  getAccountsAndTotalVoteWeight,
+  getRewards,
+  client
 };
